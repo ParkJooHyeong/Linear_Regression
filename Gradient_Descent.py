4d0c320de9bdcc82cb58660b5d54019cffa395e1
ParkJooHyeong <parkjh0856@gmail.com>
2020-07-13 17:37:38 +0900

    commit first

---
+++ Gradient_Descent.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import functions as func
+
+
+
+x=np.random.rand(100,1)
+y=4+3*x+np.random.randn(100,1)
+
+# d = func.model(x, y, num_iterations = 1000, learning_rate = 0.02, print_cost = True)
+
+# print ("w = " + str(d["w"]))
+# print ("b = " + str(d["b"]))
+#
+# y_hat = np.dot(np.transpose(d["w"]),x)+d["b"]
+plt.scatter(x, y,label='data')
+# plt.plot(np.transpose(x),np.transpose(y_hat), color='red', linewidth=2, label='predicted')
+plt.title('Linear Regression')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
